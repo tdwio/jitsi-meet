f3e36a868a91823024e77e726401372cb8d618b8
Tim DE WINTER <tim.dewinter@infomaniak.com>
2024-05-30 16:25:44 +0200

    chore: updated build/publish scripts

--- scripts/build-react-native-sdk.sh
+++ scripts/build-react-native-sdk.sh
@@ -1,26 +1,33 @@
-echo "Cleaning ..."
-rm -rf ./dist
+echo "Cleaning..."
+rm -rf ./dist ./tmp
 
-echo "Building .js and .d.ts ..."
+echo "Cloning tdwio/jitsi-react-native-sdk.git..."
+mkdir -p tmp/source
+git clone git@github.com:tdwio/jitsi-react-native-sdk.git tmp/source
+
+echo "Building .js and .d.ts..."
 tsc --project tsconfig.native.json
 tsc --project tsconfig.web.json
 
 echo "Installing original module..."
-rm -rf ./tmp
 npm i --prefix ./tmp @jitsi/react-native-sdk
 mv ./tmp/node_modules/@jitsi/react-native-sdk ./tmp/module
 
 echo "Copying files..."
-rm -rf ./tmp/node_modules ./tmp/package.json ./tmp/package-lock.json ./tmp/module/react/features ./tmp/module/index.tsx
-mv ./dist/react/features ./tmp/module/react/features
-mv ./dist/index.d.ts ./tmp/module/index.d.ts
-mv ./dist/index.js ./tmp/module/index.js
+rm -rf ./tmp/node_modules ./tmp/package.json ./tmp/package-lock.json
+find ./tmp/module -name "*.ts" -type f -delete
+find ./tmp/module -name "*.tsx" -type f -delete
+rsync -am --ignore-existing ./dist/react/features ./tmp/module/react
+cp -r ./dist/index.d.ts ./tmp/module/index.d.ts
+cp -r ./dist/index.js ./tmp/module/index.js
+
+echo "Cleaning up..."
 rm -rf ./dist
 mv ./tmp/module ./dist
+mv ./tmp/source/.git ./dist/.git
 
-echo "Preparing publish config..."
+echo "Updating package.json..."
 TMP=$(mktemp)
-jq '.name = "@tdwio/jitsi-react-native-sdk"' ./dist/package.json > "$TMP" && mv "$TMP" ./dist/package.json
-jq '.repository.url = "https://github.com/tdwio/jitsi-react-native-sdk"' ./dist/package.json > "$TMP" && mv "$TMP" ./dist/package.json
+jq '.main = "index.js"' ./dist/package.json > "$TMP" && mv "$TMP" ./dist/package.json
+jq '.types = "index.d.ts"' ./dist/package.json > "$TMP" && mv "$TMP" ./dist/package.json
 jq '.scripts = {}' ./dist/package.json > "$TMP" && mv "$TMP" ./dist/package.json
-cp ./.npmrc ./dist/.npmrc
--- scripts/publish-react-native-sdk.sh
+++ scripts/publish-react-native-sdk.sh
@@ -1,9 +1,8 @@
 cd ./dist
 
 VERSION=$(jq -r .version ./package.json)
-echo "Publishing version $VERSION..."
-npm publish
-
-echo "Pushing new tag v$VERSION..."
-git tag v$VERSION
+echo "Pushing new tag $VERSION..."
+git add -A
+git commit -m "v$VERSION"
+git tag $VERSION
 git push && git push --tags
